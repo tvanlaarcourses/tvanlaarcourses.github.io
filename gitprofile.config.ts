bb21064769e87a7e7dd07dcdf691bd3752534ece
Tricia Van Laar <tvanlaar@csustan.edu>
2026-05-12 10:11:47 -0700

    Update project title and description in config

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -33,9 +33,9 @@ const CONFIG = {
       // To hide the `External Projects` section, keep it empty.
       projects: [
         {
-          title: 'Project Name',
+          title: 'My Poster',
           description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+            'This is a poster of my research',
           imageUrl:
             'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
           link: 'https://example.com',
